3647842afaf0a15f3f541abb6edd063ac361b1ae
Menner Freshket <men@freshket.co>
2020-11-18 18:25:24 +0700

    Fix - return type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import '../i18n';
 import React, { useEffect } from 'react';
-import type { FC } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useTranslation } from 'react-i18next';
-import { Router, useRouter } from 'next/router';
-import { AppPropsType } from 'next/dist/next-server/lib/utils';
+import { useRouter } from 'next/router';
+import { NextPage } from 'next';
 
-const MyApp: FC<AppPropsType<Router, Record<string, unknown>>> = ({
-  Component,
-  pageProps,
-}: AppProps) => {
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
   const { i18n } = useTranslation();
   const router = useRouter();
   const { locale } = router;
